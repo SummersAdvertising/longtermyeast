8155d21ceba6c4fddf16214819e4f0094e7faa65
Yuzhe <yuzhe.c85@gmail.com>
2013-01-22 11:51:26 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Longtermyeast::Application.config.secret_token = '232f631949ac406d341569fdaf92bbdb772d26e7b7787120a1ea8fc30ec31ba0572421fa689fdbc4595bb17d916a5bcbc7ed0a028d5219041f7c73b040ced989'
